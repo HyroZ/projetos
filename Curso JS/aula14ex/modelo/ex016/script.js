155296c3b9351df9e56aad361b0fe713a7358ff3
hyro <batistajosue957@gmail.com>
2026-09-12 12:06:01 -0300

    adicionando contagem regressiva

--- Curso JS/aula14ex/modelo/ex016/script.js
+++ Curso JS/aula14ex/modelo/ex016/script.js
@@ -3,20 +3,26 @@ function contar() {
     let f = document.getElementById('fim').value;
     let p = document.getElementById('passo').value;
     let res = document.getElementById('res');
-    numberI = Number(i);
-    numberF = Number(f);
-    numberP = Number(p);
     if (numberI == 0 || numberF == 0 || numberP == 0) {
         res.innerHTML = 'Impossível contar!';
         alert('[ERRO] Faltam dados!');
     }
     else {
+        numberI = Number(i);
+        numberF = Number(f);
+        numberP = Number(p);
         res.innerHTML = 'Contando: <br>';
-        for (let c = numberI; c <= numberF; c += numberP) {
-            res.innerHTML += `${c} \u{1F449}`;
+        if ( numberI < numberF) {
+            for (let c = numberI; c <= numberF; c += numberP) {
+                res.innerHTML += `${c} \u{1F449}`;
+            }
+        } else {
+            for (let c = numberI; c >= numberF; c -= numberP) {
+                res.innerHTML += `${c} \u{1F449}`;
+            }
         }
-
-
+        res.innerHTML += `\u{1F3C1}`;
     }
-     res.innerHTML += `\u{1F3C1}`;
+
+    
 }
