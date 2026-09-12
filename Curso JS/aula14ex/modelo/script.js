5879da2401e3addf315b4edc9c462fb6dce8e58d
hyro <batistajosue957@gmail.com>
2026-09-12 12:23:14 -0300

    Criando tabuada em JS

--- Curso JS/aula14ex/modelo/script.js
+++ Curso JS/aula14ex/modelo/script.js
@@ -1,1 +1,16 @@
-var Dia = new Date();
+function gerarTabuada() {
+    let numero = document.getElementById("numero").value;
+    let res = document.getElementById("res");
+    res.innerHTML = "";
+    if (numero == "") {
+        res.innerHTML = "Por favor, insira um número.";
+        return;
+    }
+    else {
+        Number(numero);
+        for (let i = 1; i <= 10; i++) {
+            let resultado = numero * i;
+            res.innerHTML += `${numero} x ${i} = ${resultado}<br>`;
+        }
+    }
+}
