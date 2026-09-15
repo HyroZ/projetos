6623a401e477d7b1073ea1239853e30fe23d839f
hyro <batistajosue957@gmail.com>
2026-09-14 23:52:31 -0300

    Criando variáveis de tipos diferentes

--- Python/exercicio1.py
+++ Python/exercicio1.py
@@ -1,1 +1,11 @@
 print("Hello, World!")
+#2 Criando variáveis de tipos diferentes
+x = 10
+nome = 'Anderson'
+nota = 8.75
+fez_inscricao = True
+
+print(x)
+print(nome)
+print(nota)
+print(fez_inscricao)
