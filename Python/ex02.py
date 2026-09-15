9d26106aabb5c8930efa701206e9259859ee6a04
hyro <batistajosue957@gmail.com>
2026-09-15 00:06:00 -0300

    if e else com input (exercicio faculdade)

--- Python/ex02.py
+++ Python/ex02.py
@@ -11,3 +11,23 @@ elif idade >= 18 and idade < 60:
   print("Adulto")
 else:
   print("Idoso")
+
+idade = int(input("Digite sua idade: "))
+
+Ingressos1 = 10
+Ingressos2 = 0
+Ingressos3 = 15
+Ingressos4 = 3
+
+if idade < 12:
+  print(f"Recomendamos o filme infantil 1. Que atualmente está com {Ingressos1} Disponíveis")
+elif idade >= 12 and idade < 18:
+  print(f"Recomendamos o filme infantil 2. Que atualmente está com {Ingressos2} Disponíveis")
+elif idade >= 18 and idade < 25:
+  print(f"Recomendamos o filme adulto. Que atualmente está com {Ingressos3} Disponíveis")
+else:
+  print(f"Recomendamos o filme adulto 2. Que atualmente está com {Ingressos4} Disponíveis")
+
+
+
+
