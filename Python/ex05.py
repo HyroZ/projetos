a53d04b183f77655df3c7b837c71e7f52f448f36
hyro <batistajosue957@gmail.com>
2026-09-16 22:44:41 -0300

    soma e resultado

--- Python/ex05.py
+++ Python/ex05.py
@@ -1,3 +1,10 @@
 numero = [1, 2, 3, 4, 5]
 comprimento = len(numero)
 print(f"O comprimento da lista é: {comprimento}")
+
+def soma(a,b ,c ,d):
+    resultado = a + b + c + d
+    return resultado
+
+resultado = soma(1, 2, 3, 4)
+print(f"A soma dos números é: {resultado}")
