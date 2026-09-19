5e50d3a051a8f27d269fae2906aa315fd359a73f
hyro <batistajosue957@gmail.com>
2026-09-19 01:08:01 -0300

    len(), text,count e index() com for in

---
+++ Python/ex08.py
@@ -0,0 +1,17 @@
+texto = "explorando a diversidade de linguagens de programação com Python"
+print(f"Tamanho do texto: {len(texto)}")
+print(f"Quantidade de 'e' no texto: {texto.count('e')}")
+print(f"As 5 primeiras letras do texto: {texto[0:5]}")
+
+cores = ['v', 'a', 'l', 'o', 'h']
+
+for cor in cores:
+    print(f"Posição = {cores.index(cor)} | Cor = {cor}")
+
+linguagens = ["Python", "JavaScript", "C++", "Java", "C#"]
+print("Antes da listcomp =", linguagens)
+
+vogais = ('a', 'e', 'i', 'o', 'u')
+print(f"Tipos de vogais = {type(vogais)}")
+for p, z in enumerate(vogais):
+  print(f"Posição = {p}, valor = {z}")
