f2ecd4d89f97876aac17e41f4c5588e05826b3d3
hyro <batistajosue957@gmail.com>
2026-09-19 10:46:57 -0300

    Verificando se um elemento está no conjunto

--- Python/ex09.py
+++ Python/ex09.py
@@ -5,3 +5,10 @@ meu_conjunto.add(30)
 meu_conjunto.add(10)
 
 print(meu_conjunto)
+
+#Verificando se um elemento está no conjunto
+elemento = 20
+if elemento in meu_conjunto:
+  print(f"O elemento {elemento} está no conjunto")
+else:
+  print(f"O elemento {elemento} não está no conjunto")
