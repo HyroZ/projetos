e363d521f5809b01540827a9509d382a4ca88e1f
hyro <batistajosue957@gmail.com>
2026-09-19 10:47:59 -0300

    len() e set()

--- Python/ex09.py
+++ Python/ex09.py
@@ -12,3 +12,10 @@ if elemento in meu_conjunto:
   print(f"O elemento {elemento} está no conjunto")
 else:
   print(f"O elemento {elemento} não está no conjunto")
+
+#Numero de elementos repetidos e únicos em uma lista
+numeros_rep = [1,2,2,2,3,3,3,4,4,4]
+unicos = set(numeros_rep)
+print(f"Elementos únicos: {unicos}")
+print(f"Numero de elementos únicos: {len(unicos)}")
+print(f"Numero de elementos repetidos: {len(numeros_rep) - len(unicos)}")
