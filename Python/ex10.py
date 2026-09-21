fe7e90af57a71a2c9a39d7d71fd1e48022a758d6
hyro <batistajosue957@gmail.com>
2026-09-20 22:45:12 -0300

    Acessando valores em um dicionário

---
+++ Python/ex10.py
@@ -0,0 +1,17 @@
+dici_1 = {}
+dici_1['nome'] = "Maria"
+dici_1['idade'] = 25
+dici_1['cidade'] = "São Paulo"
+
+print(dici_1)
+
+dici_2 = {'nome': 'Maria', 'idade': 25, }
+print(dici_2)
+
+#Acessando e alterando valores num dicionário
+produto = {'nome': 'Notebook', 'preco': 3500.00, 'estoque': 12}
+print(f"Nome do produto: {produto['nome']}")
+print(f"Preço do produto: {produto['preco']}")
+print(f"Quantidade em estoque: {produto['estoque']}")
+
+x
