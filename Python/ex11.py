6a8eee777547aadd6d84dd94292316dffa8fc2d4
hyro <batistajosue957@gmail.com>
2026-09-20 22:47:57 -0300

    Operações em massa

--- Python/ex11.py
+++ Python/ex11.py
@@ -3,3 +3,12 @@ import numpy as np
 my_array = np.array([1, 2, 3, 4, 5])
 print("Array original")
 print(my_array)
+
+#NumPy - Operações em massa
+squared_array = my_array ** 2   #elemento ao quadrado
+sum_of_elements = np.sum(my_array) #soma dos elementos
+
+print("\nArray ao quadrado")
+print(squared_array)
+print("\nSoma dos elementos")
+print(sum_of_elements)
