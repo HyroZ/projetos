fa9410b76da7380a8e01dc876240dd4fb3a38c79
hyro <batistajosue957@gmail.com>
2026-09-21 22:47:18 -0300

    Evento científico com mapping

---
+++ Python/ex12.py
@@ -0,0 +1,13 @@
+#O evento científico
+import numpy as np
+
+#Cada participante é um Dicionário (mapping)
+participantes = [
+    {'nome': 'Alice', 'localizacao': "EUA", 'afiliacao': "Universidade A",
+     "interesses": ["Física", "Astronomia"]},
+    {'nome': 'Bob', 'localizacao': "Brasil", 'afiliacao': "Instituicao B",
+     "interesses": ["Biologia", "Astronomia"]},
+    {'nome': 'Charlie', 'localizacao': "Índia", 'afiliacao': "Instituto C",
+     "interesses": ["Química", "Engenharia"]}
+]
+
